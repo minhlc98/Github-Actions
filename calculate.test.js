d542b3e738f0240082e1c6595b656bf854e39ef9
Cam Minh <minhlc98@gmail.com>
2022-11-19 10:45:03 +0700

    add case fail test

--- calculate.test.js
+++ calculate.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 10 + 12 to equal 22', () => {
   expect(sum(10, 12)).toBe(22);
 });
+
+test('add 1 + 1 to equal 2', () => {
+  expect(sum(1, 1)).toBe(3);
+});
