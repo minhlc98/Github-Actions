2e282d56f579bacc7d17a2c6298562067ee39306
Cam Minh <minhlc98@gmail.com>
2022-11-19 11:44:49 +0700

    trigger build

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.json({ success: true }));
+app.get('/', (req, res) => res.json({ build_success: true }));
+
+app.get('/enviroment', (req, res) => res.json({ enviroment: process.env.NODE_ENV || '' }));
 
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
